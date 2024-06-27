f95759aa0ccb37945f34cc04713a3e403cd73703
Prathmesh Chavan <96901316+prathmchavan@users.noreply.github.com>
2024-06-27 22:32:22 +0530

    error handling section done

--- backend/auth/src/index.ts
+++ backend/auth/src/index.ts
@@ -1,9 +1,11 @@
 import express, { Application , Response , Request, Router} from "express"
 import router from "./routes";
+import { errorHandler } from "./middlewares/error-handler";
 
 let app : Application = express();
 let port : number = 8000
 app.use("/",router);
+app.use(errorHandler);
 
 
 
---
+++ backend/auth/src/middlewares/error-handler.ts
@@ -0,0 +1,61 @@
+import { Request, Response, NextFunction } from 'express'
+import { ValidationError } from 'express-validator';
+import { CustomError } from '../errors/customError';
+
+
+
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+
+    console.log("something went wrong ", err);
+
+    if(err instanceof CustomError) {
+
+      
+        return res.status(err.statusCode).send({errors: err.serializeErrors()}); 
+    }
+
+
+
+
+    res.status(400).send({ message: err.message });
+}
+
+
+
+export class RequestValidationError extends CustomError {
+
+    statusCode  = 400;
+
+    constructor(public errors: ValidationError[]) {
+        super("request error");
+        Object.setPrototypeOf(this, RequestValidationError.prototype);
+    }
+    serializeErrors()
+    {
+        return this.errors.map(error=>{
+            return{ message: error.msg , field : error.type}
+        })
+    }
+}
+
+export class DatabaseConnectionError extends CustomError {
+    statusCode = 500;
+    reason = 'error connecting to database'
+    constructor() {
+        super("database error ");
+
+        Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+
+    serializeErrors()
+    {
+        return [
+            {message : this.reason}
+        ]
+    }
+}
+
+
+
+
+
